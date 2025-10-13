fba1958d38c7256741103b6e36d2b4ad8faee8e3
sgc <yokomori@sgc-gold.co.jp>
2025-10-13 20:39:50 +0900

    Update exhibition.js

--- exhibition.js
+++ exhibition.js
@@ -1,11 +1,14 @@
+import { Calendar } from 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/main.esm.js';
+import jaLocale from 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/locales/ja.js';
+
 document.addEventListener("DOMContentLoaded", async () => {
   const res = await fetch("data/exhibition.json");
   const exhibitions = await res.json();
 
   const calendarEl = document.getElementById("calendar");
-  const calendar = new FullCalendar.Calendar(calendarEl, {
+  const calendar = new Calendar(calendarEl, {
     initialView: "dayGridMonth",
-    locale: "ja",
+    locale: jaLocale,
     events: exhibitions.map(e => ({
       title: e.title,
       start: e.start,
@@ -22,6 +25,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       document.getElementById("eventModal").classList.remove("hidden");
     }
   });
+
   calendar.render();
 
   document.getElementById("closeModal").onclick = () =>
